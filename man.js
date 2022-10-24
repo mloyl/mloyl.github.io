33878cbe21d4523854d28575a9fd50a39b5092d8
mloyl <96318821+mloyl@users.noreply.github.com>
2022-10-24 18:25:56 -0400

    Update man.js

--- man.js
+++ man.js
@@ -10,20 +10,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   red = color(255,200,200);
   green = color(200,255,200);
-  
-  
-    const firebaseConfig = {
-    apiKey: "AIzaSyA9Rjl3IVUyuCAG7x6-DDjq63bePQFqh-k",
-    authDomain: "man-in-the-house.firebaseapp.com",
-    projectId: "man-in-the-house",
-    storageBucket: "man-in-the-house.appspot.com",
-    messagingSenderId: "951086785725",
-    appId: "1:951086785725:web:72467acffc4f71cd2803d4",
-    measurementId: "G-FY20SSC7PT"
-  
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  consol.log(firebase);
 }
 
 function draw() {
